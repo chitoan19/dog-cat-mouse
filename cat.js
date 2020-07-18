a0a6bf0c597aa9cb22b597acdaf58766a98a1eff
ChiToan <chitoan1199@gmail.com>
2020-07-18 15:45:37 +0700

    add dog and cat

--- cat.js
+++ cat.js
@@ -1,7 +1,7 @@
 function Cat(){
 	this.stomach = [];
 }
-
+//test
 Cat.prototype.eat = function(mouse) {
 	this.stomach.push(mouse);
 };
--- dog.js
+++ dog.js
@@ -1,7 +1,7 @@
 function dog(){
 	this.stomach = [];
 }
-
+//test
 dog.prototype.eat(cat){
 	this.stomach.push(cat);
 };
